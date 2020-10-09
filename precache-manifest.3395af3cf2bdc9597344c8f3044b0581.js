5fed2dd5a5f2bdaaf25eb088ee685dbf0eca7e9c
RutujaMGholap <rutujamgholap99@gmail.com>
2020-10-09 17:24:25 +0530

    Updates

---
+++ precache-manifest.3395af3cf2bdc9597344c8f3044b0581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3bbc488d1e6f2e4aea956209d8abce",
+    "url": "/atmanirbhar_farmportal/index.html"
+  },
+  {
+    "revision": "c5daa9f2b3ff3c1eaca9",
+    "url": "/atmanirbhar_farmportal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc4b1646effd7e99c9f9",
+    "url": "/atmanirbhar_farmportal/static/js/2.c14eb6cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/atmanirbhar_farmportal/static/js/2.c14eb6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5daa9f2b3ff3c1eaca9",
+    "url": "/atmanirbhar_farmportal/static/js/main.170e9908.chunk.js"
+  },
+  {
+    "revision": "4396cf456a9090b0d777",
+    "url": "/atmanirbhar_farmportal/static/js/runtime-main.bacb1de4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/atmanirbhar_farmportal/static/media/logo.5d5d9eef.svg"
+  }
+]);
