2309d3a6363cc94566f3f34f51bb0d18b78e6815
RutujaMGholap <rutujamgholap99@gmail.com>
2020-10-12 11:56:00 +0530

    Additional changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Main from './components/MainComponent';
+import {BrowserRouter} from 'react-router-dom';
+class App extends Component{
 
-function App() {
+ render(){ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div>
+    <Main/>
     </div>
+    </BrowserRouter>
+      
   );
+}
 }
 
 export default App;
+
